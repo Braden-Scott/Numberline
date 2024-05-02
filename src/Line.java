01d21d2ac5a83938df7e7783c8c1968c2d72e561
Braden_Scott <braden.p.scott01@gmail.com>
2024-05-02 07:40:58 -0700

    Added Line Class

---
+++ src/Line.java
@@ -0,0 +1,14 @@
+public class Line {
+    public Object[] array;
+    
+    public Line() {
+        array = new Object[11];
+    }
+
+    public Line(int size) {
+        array = new Object[size];
+    }
+
+    
+
+}
